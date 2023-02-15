c1e3182e0a5df745cbe19daf96fd02aa06cec41f
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-15 17:51:45 +0200

    refactoring to react-toolkit

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -3,7 +3,3 @@ export const statusFilters = Object.freeze({
   active: 'active',
   completed: 'completed',
 });
-
-// Используем метод Object.freeze() для того, чтобы «заморозить»
-// объект значений фильтра и предотвратить
-// случайное его изменение по ссылке в местах импорта.
