7d674b1a5833b8c7a6b016e6a6b5b5cb325d2324
Denys Borysiuk <denisborisiuk@gmail.com>
2024-03-22 15:38:01 +0200

    optimize counter code

--- src/components/TaskCounter/TaskCounter.tsx
+++ src/components/TaskCounter/TaskCounter.tsx
@@ -1,38 +1,17 @@
-import { useEffect, useState } from 'react';
-
 import { useSelector } from 'react-redux';
 
-import { selectAllTasks } from '@/redux/tasks/selectors';
-
-import { ITask } from '@/types';
+import { selectTaskCount } from '@/redux/tasks/selectors';
 
 import css from './TaskCounter.module.css';
 
 export const TaskCounter = () => {
-  const tasks = useSelector(selectAllTasks);
-  const [counter, setCounter] = useState({ active: 0, completed: 0 });
-
-  useEffect(() => {
-    const count = tasks.reduce(
-      (acc, task: ITask) => {
-        if (task.completed) {
-          acc.completed += 1;
-        } else {
-          acc.active += 1;
-        }
-        return acc;
-      },
-      { active: 0, completed: 0 }
-    );
-
-    setCounter(count);
-  }, [tasks]);
+  const count = useSelector(selectTaskCount);
 
   return (
     <div>
-      <p className={css.text}>Active: {counter.active}</p>
+      <p className={css.text}>Active: {count.active}</p>
 
-      <p className={css.text}>Completed: {counter.completed}</p>
+      <p className={css.text}>Completed: {count.completed}</p>
     </div>
   );
 };
--- src/redux/tasks/selectors.ts
+++ src/redux/tasks/selectors.ts
@@ -19,3 +19,19 @@ export const selectVisibleTasks = (state: RootState) => {
       return tasks;
   }
 };
+
+export const selectTaskCount = (state: RootState) => {
+  const tasks = selectAllTasks(state);
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+};
