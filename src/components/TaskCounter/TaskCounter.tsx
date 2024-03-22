8920d26cbf675d97cbe0e6603f48d7e394345e99
Denys Borysiuk <denisborisiuk@gmail.com>
2024-03-22 16:16:23 +0200

    refactoring task counter

--- src/components/TaskCounter/TaskCounter.tsx
+++ src/components/TaskCounter/TaskCounter.tsx
@@ -5,13 +5,13 @@ import { selectTaskCount } from '@/redux/tasks/selectors';
 import css from './TaskCounter.module.css';
 
 export const TaskCounter = () => {
-  const count = useSelector(selectTaskCount);
+  const { active, completed } = useSelector(selectTaskCount);
 
   return (
     <div>
-      <p className={css.text}>Active: {count.active}</p>
+      <p className={css.text}>Active: {active}</p>
 
-      <p className={css.text}>Completed: {count.completed}</p>
+      <p className={css.text}>Completed: {completed}</p>
     </div>
   );
 };
